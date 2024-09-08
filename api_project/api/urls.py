f0c22aa58e6ff66ad5003b93bf254ad90d28e0f2
Ephraim Mucale <ephraimmukhale@gmail.com>
2024-09-08 23:40:12 +0200

    New commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -8,8 +8,14 @@ from . import views
 
 router = DefaultRouter()
 router.register(r'books', viewset=views.BookViewSet)
+router.register(r'detail', viewset=views.DetailViewSet)
+router.register(r'create', viewset=views.CreateViewSet)
+router.register(r'update', viewset=views.UpdateViewSet)
+router.register(r'delete', viewset=views.DeleteViewSet)
+
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path(' ', 'BookList/', views.Booklist),
+    path('api/', include(router.urls)),
 ]
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,8 +4,26 @@ from rest_framework.response import Response
 from .serializers import BookSerializer
 from rest_framework import viewsets
 from .models import Book
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class Booklist(viewsets.ModelViewSet):
+class Booklist(generics.ListAPIView):
    queryset = Book.objects.all()
-   serializer = BookSerializer
+   serializer_class = BookSerializer
     
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CreateView(generics.CreateAPIView):
+     queryset = Book.objects.all()
+     serializer_class = BookSerializer
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = IsAuthenticatedOrReadOnly
